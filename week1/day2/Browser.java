0476d080985a6b4e369c6287ea80eaefde0960cd
yuvanshankarpandian <yuvanshankarpandian.v2003@gmail.com>
2024-11-15 12:18:18 +0530

    Add files via upload
    week 1

---
+++ week1/day2/Browser.java
@@ -0,0 +1,29 @@
+package week1.day2;
+
+ public class Browser {
+	
+ 
+
+ public static void main(String[] args) {
+
+      Browser a = new Browser();
+      String launchBrowser =a.launchBrowser("kaipulla");
+      System.out.println(launchBrowser);
+
+       a.loadurl();
+ }
+
+ 
+
+
+public String launchBrowser(String browsername) {
+	System.out.println(browsername);
+return browsername;
+
+}
+
+public void loadurl() { 
+         System.out.print("application URL loaded");
+
+ }
+ }
